167478a75de25180749a5d7d0a82a1fad65fde40
KeeganNaidoo <69624407+KeeganNaidoo@users.noreply.github.com>
2024-12-02 23:10:06 +0200

    Final Fixes for Game
    Added Procedurally generated feature for snow fall

--- Assets/Scripts/SnowSpawner.cs
+++ Assets/Scripts/SnowSpawner.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class SnowSpawner : MonoBehaviour
 {
+    public GameObject snowflakePrefab; // Assign the snowflake prefab
+    public float spawnRate = 0.001f;     // Time between spawns
+    public Vector3 spawnArea = new Vector3(150f, 10f, 150f); // X and Z spawn area size
+    public float fallSpeed = 5f;       // Speed of snowflake fall
+    public float lifetime = 5f;       // Lifetime of each snowflake
+
+    private float timer = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,34 @@ public class SnowSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
+
+        if (timer >= spawnRate)
+        {
+            SpawnSnowflake();
+            timer = 0f;
+        }
+    }
+    
+    void SpawnSnowflake()
+    {
+        Vector3 spawnPosition = new Vector3(
+            Random.Range(-spawnArea.x / 2, spawnArea.x / 2),
+            spawnArea.y,
+            Random.Range(-spawnArea.z / 2, spawnArea.z / 2)
+        );
+
+        GameObject snowflake = Instantiate(snowflakePrefab, spawnPosition, Quaternion.Euler(0, Random.Range(0, 360), 0));
+        snowflake.transform.localScale *= Random.Range(0.5f, 1.5f);
+
+        Rigidbody rb = snowflake.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.velocity = new Vector3(0, -fallSpeed, 0);
+        }
+
+        Destroy(snowflake, lifetime);
         
+        //rb.velocity = new Vector3(Random.Range(-1f, 1f), -fallSpeed, Random.Range(-1f, 1f));
     }
 }
